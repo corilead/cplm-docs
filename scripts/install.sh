958ec608c9bb66c0dbeacf76b694812669f1cfd0
Li Feixiang <fli@corilead.com>
2020-07-07 01:10:33 +0800

    install

--- scripts/install.sh
+++ scripts/install.sh
@@ -98,6 +98,7 @@ sudo systemctl daemon-reload
 sudo systemctl enable elasticsearch
 sudo systemctl start elasticsearch
 
+echo 'Install MongoDB'
 cd ~
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-rhel70-4.2.8.tgz
 
@@ -143,3 +144,61 @@ EOF'
 sudo systemctl daemon-reload
 sudo systemctl enable mongodb
 sudo systemctl start mongodb
+
+echo 'Install Nginx'
+cd ~
+wget https://ftp.pcre.org/pub/pcre/pcre-8.44.tar.gz
+wget http://zlib.net/zlib-1.2.11.tar.gz
+wget http://www.openssl.org/source/openssl-1.1.1g.tar.gz
+wget https://nginx.org/download/nginx-1.18.0.tar.gz
+
+cd ~
+tar -zxf pcre-*.tar.gz
+cd pcre-*
+./configure
+make
+sudo make install
+
+cd ~
+tar -zxf zlib-*.tar.gz
+cd zlib-*
+./configure
+make
+sudo make install
+
+cd ~
+tar -zxf openssl-*.tar.gz
+cd openssl-*
+./Configure linux-x86_64 --prefix=/usr
+make
+sudo make install
+
+cd ~
+tar zxf nginx-*.tar.gz
+cd nginx-*
+./configure --prefix=/usr/local/nginx --with-pcre=../pcre-8.44 --with-zlib=../zlib-1.2.11 --with-openssl=../openssl-1.1.1g --with-http_ssl_module --with-stream
+make
+sudo make install
+
+sudo bash -c 'cat << EOF > /usr/lib/systemd/system/nginx.service
+[Unit]
+Description=The NGINX HTTP and reverse proxy server
+After=syslog.target network-online.target remote-fs.target nss-lookup.target
+Wants=network-online.target
+
+[Service]
+Type=forking
+ExecStartPre=/usr/local/nginx/sbin/nginx -t
+ExecStart=/usr/local/nginx/sbin/nginx
+ExecReload=/usr/sbin/nginx -s reload
+ExecStop=/bin/kill -s QUIT $MAINPID
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target
+EOF'
+
+
+sudo systemctl daemon-reload
+sudo systemctl enable nginx
+sudo systemctl start nginx
