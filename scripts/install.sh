a1a858c3ebf480ac3256bfc998736ed16f9d825d
Li Feixiang <fli@corilead.com>
2020-07-07 00:49:13 +0800

    install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -43,3 +43,102 @@ sudo sed -i 's/$CLIEXEC -p $REDISPORT shutdown/$CLIEXEC -a password -p $REDISPOR
 sudo chkconfig redis_6379 on
 sudo service redis_6379 start
 
+cd ~
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.4.3.tar.gz
+sudo tar -xzf elasticsearch-*.tar.gz -C /usr/local
+sudo mv /usr/local/elasticsearch-* /usr/local/elasticsearch
+sudo chown -R cplm: /usr/local/elasticsearch
+
+sudo bash -c 'cat << EOF > /tmp/elasticsearch.service
+[Unit]
+Description=Elasticsearch
+Documentation=http://www.elastic.co
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+PrivateTmp=true
+User=cplm
+
+ExecStart=/usr/local/elasticsearch/bin/elasticsearch -p /usr/local/elasticsearch/logs/elasticsearch.pid --quiet
+
+# Specifies the maximum file descriptor number that can be opened by this process
+LimitNOFILE=65536
+
+# Specifies the maximum number of processes
+LimitNPROC=4096
+
+# Specifies the maximum size of virtual memory
+LimitAS=infinity
+
+# Specifies the maximum file size
+LimitFSIZE=infinity
+
+# Disable timeout logic and wait until process is stopped
+TimeoutStopSec=0
+
+# SIGTERM signal is used to stop the Java process
+KillSignal=SIGTERM
+
+# Send the signal only to the JVM rather than its control group
+KillMode=process
+
+# Java process is never killed
+SendSIGKILL=no
+
+# When a JVM receives a SIGTERM signal it exits with code 143
+SuccessExitStatus=143
+
+[Install]
+WantedBy=multi-user.target
+EOF'
+
+sudo systemctl daemon-reload
+sudo systemctl enable elasticsearch
+sudo systemctl start elasticsearch
+
+cd ~
+wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-rhel70-4.2.8.tgz
+
+sudo tar -zxvf mongodb-linux-*.tgz -C /usr/local
+sudo mv /usr/local/mongodb-* /usr/local/mongodb
+sudo chown -R cplm: /usr/local/mongodb
+
+sudo mkdir -p /var/lib/mongodb
+sudo mkdir -p /var/log/mongodb
+sudo chown -R cplm: /var/lib/mongodb
+sudo chown -R cplm: /var/log/mongodb
+
+sudo bash -c 'cat << EOF > /usr/local/etc/mongod.conf
+systemLog:
+  destination: file
+  path: /var/log/mongodb/mongo.log
+  logAppend: true
+storage:
+  dbPath: /var/lib/mongodb
+net:
+  bindIpAll: true
+EOF'
+
+sudo bash -c 'cat << EOF > /usr/lib/systemd/system/mongodb.service
+[Unit]
+Description=MongoDB Database Service
+Wants=network.target
+After=network.target
+
+[Service]
+Type=forking
+ExecStart=/usr/local/mongodb/bin/mongod --config /usr/local/etc/mongod.conf --fork
+ExecReload=/bin/kill -HUP $MAINPID
+Restart=always
+User=cplm
+StandardOutput=syslog
+StandardError=syslog
+
+[Install]
+WantedBy=multi-user.target
+EOF'
+
+sudo systemctl daemon-reload
+sudo systemctl enable mongodb
+sudo systemctl start mongodb
